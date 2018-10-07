611ad1bbaa26cf9e0e11bfce5a2103a451f0009f
piechocki <34336439+piechocki@users.noreply.github.com>
2018-10-07 13:40:18 +0200

    Make solution cleaner and do some minor bug fixes

--- Util/pandas_helper.py
+++ Util/pandas_helper.py
@@ -13,30 +13,6 @@ engine = "c"
 #d = lambda x: pd.to_datetime(x)
 #converters=None #{'Price': n, 'Volume': n}
 
-#def get_date_with_last_row(source, first_row):
-
-#    skip = first_row-1 if first_row > 0 else 0
-#    date = pd.read_csv(source, compression=compression, header=header, sep=',', quotechar='"', nrows=1, skiprows=skip, low_memory=low_memory, na_filter=na_filter, names=names, engine=engine, usecols=["Date[G]"])["Date[G]"][0]
-#    while True:
-#        first_row += chunk_size
-#        date_new = pd.read_csv(source, compression=compression, header=header, sep=',', quotechar='"', nrows=1, skiprows=first_row-1, low_memory=low_memory, na_filter=na_filter, names=names, engine=engine, usecols=["Date[G]"])["Date[G]"][0]
-#        if date != date_new:
-#            break
-#    chunk = pd.read_csv(source, compression=compression, header=header, sep=',', quotechar='"', nrows=chunk_size, skiprows=first_row-1-chunk_size, low_memory=low_memory, na_filter=na_filter, names=names, engine=engine, usecols=["Date[G]"])
-#    for i in range(chunk_size):
-#        if date != chunk["Date[G]"][i]:
-#            last_row = first_row - 1 - chunk_size + i
-#            print(date)
-#            return str(date), last_row
-    
-#    #for chunk in pd.read_csv(source, compression='gzip', header=0, sep=',', quotechar='"', chunksize=chunk_size, skiprows=first_row, usecols=["Date[G]"]):
-#    #    if date != chunk["Date[G]"].tail(1)[0]:
-#    #        for i in range(chunk_size):
-#    #            if date != chunk["Date[G]"][i]:
-#    #                last_row = chunk.index[chunk[i]] - 1
-#    #                return date, last_row
-#    return None, None
-
 def get_dates_with_first_row(source):
 
     chunksize = 5000
@@ -49,7 +25,6 @@ def get_dates_with_first_row(source):
                 if date != row["Date[G]"].iloc[i]:
                     date = row["Date[G]"].iloc[i]
                     dates[str(date)] = int(row.index.values.astype(int)[i])
-                    print(date)
                     break
     return dates
 
