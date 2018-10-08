76a15ed872e1ae38e8e3581d0d2214d8429bf3cf
piechocki <34336439+piechocki@users.noreply.github.com>
2018-10-08 16:40:51 +0200

    Try new reading procedure to reduce times for accessing a csv file

--- Common/preprocessing.py
+++ Common/preprocessing.py
@@ -1,7 +1,9 @@
 import os
 import re
 import json
+import math
 import Util.pandas_helper as pandas_helper
+
 
 class PreProcessor:
 
@@ -14,6 +16,7 @@ class PreProcessor:
                 self.files.append(file)
         self.rows = {}
         self.aggregations = pandas_helper.get_empty_aggregation()
+
     def init_rows_per_date(self):
 
         rows = {}
@@ -26,18 +29,31 @@ class PreProcessor:
 
     def get_dataframe_per_date(self, ticker, date, date_next):
         
-        if self.rows is None:
+        if not self.rows:
             return None
-        r = re.compile(r"(\w*)_" + ticker + "_(\w*)")
-        file = list(filter(r.match, self.files))[0]
-        source = self.input_folder + file
+        source = self.get_source_by_ticker(ticker)
         first_row = self.rows.get(ticker, None).get(date, None)
         last_row = (self.rows.get(ticker, None).get(date_next, None) - 1) if date_next is not None else (-1)
         return pandas_helper.get_dataframe_by_rows(source, first_row, last_row)
 
-    def get_filtered_dataframe(self, df):
+    def get_source_by_ticker(self, ticker):
+
+        r = re.compile(r"(\w*)_" + ticker + "_(\w*)")
+        file = list(filter(r.match, self.files))[0]
+        return self.input_folder + file
+
+    def get_dataframe_per_iter(self, ticker, iter):
+
+        if not self.rows:
+            return None
+        source = self.get_source_by_ticker(ticker)
+        return pandas_helper.get_dataframe_by_iter(source, iter)
+
+    def get_filtered_dataframe(self, df, type):
         
-        df.query("Type=='Trade'", inplace=True)
+        if type != "Trade" and type != "Quote":
+            return None
+        df.query("Type=='" + type + "'", inplace=True)
         df.query("Qualifiers.str.startswith(' [ACT_FLAG1]')", inplace=True)
         df.loc[:,"Price"] = df["Price"].astype(float)
         df.loc[:,"Volume"] = df["Volume"].astype(int)
@@ -45,17 +61,48 @@ class PreProcessor:
         #df.reset_index(inplace=True, drop=True)
         return df
 
+    def get_splitted_dataframes(self, df, last_tail_length):
+
+        date = df["Date[G]"].iloc[-1]
+        ticker = df["#RIC"].iloc[0]
+        index = self.rows[ticker][date]
+        row = index % pandas_helper.rows_limit_per_iter + last_tail_length
+        print(df["Date[G]"].iloc[row-2])
+        print(df["Date[G]"].iloc[row-1])
+        print(df["Date[G]"].iloc[row])
+        print(df["Date[G]"].iloc[row+1])
+        print(df["Date[G]"].iloc[row+2])
+        return df[:row, :], df[row:, :]
+
     def get_aggregations(self):
 
         for i in range(len(self.rows)):
             ticker = list(self.rows)[i]
-            for j in range(len(self.rows[ticker])):
-                print("Processing date " + str(j+1) + " of " + str(len(self.rows[ticker])) + " in file " + str(i+1) + " of " + str(len(self.rows)) + " ...")
-                date = list(self.rows[ticker])[j]
-                date_next = list(self.rows[ticker])[j+1] if j+1 < len(self.rows[ticker]) else None
-                df = self.get_dataframe_per_date(ticker, date, date_next)
-                df = self.get_filtered_dataframe(df)
+            count_rows = self.rows[ticker][self.rows[ticker].keys()[-1]]
+            max_iter = math.ceil(count_rows / pandas_helper.rows_limit_per_iter)
+            for j in range(max_iter):
+                print("Processing iteration " + str(j+1) + " of " + str(max_iter) + " in file " + str(i+1) + " of " + str(len(self.rows)) + " ...")
+                if j == 0:
+                    df_tail = pandas_helper.pd.DataFrame()
+                df = self.get_dataframe_per_iter(ticker, j)
+                df = self.get_filtered_dataframe(df, "Trade")
+                df = pandas_helper.concat_dfs(df, df_tail)
+                if j < max_iter-1:
+                    last_tail_length = len(df_tail)
+                    df, df_tail = self.get_splitted_dataframes(df, last_tail_length)
                 self.get_aggregation(df)
+
+    #def get_aggregations(self):
+
+    #    for i in range(len(self.rows)):
+    #        ticker = list(self.rows)[i]
+    #        for j in range(len(self.rows[ticker])):
+    #            print("Processing date " + str(j+1) + " of " + str(len(self.rows[ticker])) + " in file " + str(i+1) + " of " + str(len(self.rows)) + " ...")
+    #            date = list(self.rows[ticker])[j]
+    #            date_next = list(self.rows[ticker])[j+1] if j+1 < len(self.rows[ticker]) else None
+    #            df = self.get_dataframe_per_date(ticker, date, date_next)
+    #            df = self.get_filtered_dataframe(df, "Trade")
+    #            self.get_aggregation(df)
 
     def get_aggregation(self, df):
 
@@ -74,6 +121,7 @@ class PreProcessor:
         
         f = open("rows.json")
         self.rows = json.load(f)
+        f.close()
 
     def save_aggregations_to_csv(self):
 
--- Util/pandas_helper.py
+++ Util/pandas_helper.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
 
 names = ['#RIC', 'Date[G]', 'Time[G]', 'GMT Offset', 'Type', 'Ex/Cntrb.ID', 'Price', 'Volume', 'Bid Price', 'Bid Size', 'Ask Price', 'Ask Size', 'Qualifiers']    
 chunk_size = 20000
 header = 0
 compression = "gzip" # "infer"
+rows_limit_per_iter = 5000000
 na_filter = False
 low_memory = True
 engine = "c"
@@ -34,6 +36,12 @@ def get_dataframe_by_rows(source, first_row, last_row):
     nrows = (last_row - first_row + 1) if last_row > 0 else None
     return pd.read_csv(source, engine="c", header=None, compression="gzip", na_filter=False, nrows=nrows, skiprows=skiprows, names=names, converters=converters, low_memory=False)
     
+def get_dataframe_by_iter(source, iter):
+
+    skiprows = iter * rows_limit_per_iter
+    nrows = rows_limit_per_iter
+    return pd.read_csv(source, engine="c", header=None, compression="gzip", na_filter=False, nrows=nrows, skiprows=skiprows, names=names, converters=converters, low_memory=False)
+
 def get_empty_aggregation():
 
     return pd.DataFrame({
@@ -42,7 +50,6 @@ def get_empty_aggregation():
         'V': [],
         'sigma_r': [],
         'sigma_p': [],
-        'W': [],
         'p': [],
         'P': [],
         'N': [],
@@ -52,40 +59,73 @@ def get_empty_aggregation():
 
 def get_new_aggregation(df):
     
+    #grouped = df.groupby("Date[G]")
+    #ticker = grouped["#RIC"].agg(lambda x: x.iloc[-1])
+    #V = grouped["V"].agg([np.sum])
+    #sigma_r = grouped["Return"].agg([np.std])
+    #sigma_p = grouped["Price"].agg([np.std])   
+    #x = grouped["Volume"].agg([np.mean])
+    #X = grouped["Volume"].agg([np.sum])
+    #p = grouped["Time delta * P"].agg([np.sum]) / grouped["Time delta"].agg([np.sum])
+    #P = grouped["Price"].agg([np.last])
+    #date = grouped["Date[G]"].agg(lambda x: x.iloc[0])
+    #N = grouped["#RIC"].agg([np.count()])
+
     ticker = df["#RIC"].iloc[0]
-    date = str(df["Date[G]"].iloc[0])
 
-    df["V"] = df["Price"] * df["Volume"]
-    V = df["V"].sum()
+    tickers = []
+    dates = []
+    Vs = []
+    sigma_rs = []
+    sigma_ps = []
+    ps = []
+    Ps = []
+    Ns = []
+    xs = []
+    Xs = []
 
-    df["Price-1"] = df["Price"].shift(1)
-    df["Return"] = df["Price"]/df["Price-1"]
-    sigma_r = df["Return"].sem()
+    for name, group in grouped:
+        
+        group["V"] = group["Price"] * group["Volume"]
+        group["Price-1"] = group["Price"].shift(1) # TODO: ersten (letzten?) wert je datum löschen
+        group["Return"] = group["Price"]/group["Price-1"]
+        group["Time[G]+1"] = group["Time[G]"].shift(-1) # TODO: ersten (letzten?) wert je datum löschen
+        group["Time delta"] = (group["Time[G]+1"]-group["Time[G]"]).astype('timedelta64[ms]')    
+        group["Time delta * P"] = group["Time delta"] * group["Price"]
+        
+        date = str(name)
+        V = group["V"].sum()
+        sigma_r = group["Return"].sem()
+        sigma_p = group["Price"].sem()    
+        x = group["Volume"].mean()
+        X = group["Volume"].sum()
+        p = group["Time delta * P"].sum() / group["Time delta"].sum()
+        P = group["Price"].iloc[-1]
+        date = str(group["Date[G]"].iloc[0])
+        N = len(group)
 
-    sigma_p = df["Price"].sem()
-    W = V * sigma_r
-    P = df["Price"].iloc[-1]
-    N = len(df)
-    x = df["Volume"].mean()
-    X = df["Volume"].sum()
-
-    df["Time[G]+1"] = df["Time[G]"].shift(-1)
-    df["Time delta"] = (df["Time[G]+1"]-df["Time[G]"]).astype('timedelta64[ms]')    
-    df["Time delta * P"] = df["Time delta"] * df["Price"]
-    p = df["Time delta * P"].sum() / df["Time delta"].sum()
+        tickers.append(ticker)
+        dates.append(date)
+        Vs.append(V)
+        sigma_rs.append(sigma_r)
+        sigma_ps.append(sigma_p)
+        ps.append(p)
+        Ps.append(P)
+        Ns.append(N)
+        xs.append(x)
+        Xs.append(X)
 
     return pd.DataFrame({
-        'ticker': [ticker],
-        'date': [date],
-        'V': [V],
-        'sigma_r': [sigma_r],
-        'sigma_p': [sigma_p],
-        'W': [W],
-        'p': [p],
-        'P': [P],
-        'N': [N],
-        'x': [x],
-        'X': [X]
+        'ticker': [tickers],
+        'date': [dates],
+        'V': [Vs],
+        'sigma_r': [sigma_rs],
+        'sigma_p': [sigma_ps],
+        'p': [ps],
+        'P': [Ps],
+        'N': [Ns],
+        'x': [xs],
+        'X': [Xs]
         })
 
 def concat_dfs(df1, df2):
